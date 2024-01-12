f122fd6f1a4ae069c98dfad665e6afa923bbde79
adityakumar79915 <adityakumar79915@gmail.com>
2024-01-12 11:04:41 +0530

    updated

---
+++ linked_list/linked_list.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* next;
+};
+int main(){
+    Node* n1 = new Node();
+    cout<< n1 ->data<<endl;
+    cout<< n1 ->next<<endl;
+
+    return 0;
+}
+//linked list : collection of nodes which is linear data structure.
+//node containn data and address of another node.
+//why : array doesnot change during runntime.
+//linked list is dynamic data structure.
+//insretion and deletion is easy in linkeed list.
+//types of linkeed list : 1> singly linked list , 2> doubly linked list, 3> circular linked list, 4> circular doubly linked list
