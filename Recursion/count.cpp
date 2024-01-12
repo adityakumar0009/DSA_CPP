c58d72e2859291f304e65f16648004c7a0386cc2
adityakumar79915 <adityakumar79915@gmail.com>
2024-01-12 23:11:15 +0530

    updated

---
+++ Recursion/count.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+void count(int n){
+    //base case
+    if(n==0){
+        return ;
+    }
+    //recursive relation
+    count(n-1);
+    cout<<n<<endl;
+    
+}
+int main(){
+    int n;
+    cin>>n;
+    cout<<endl;
+    count(n);
+    return 0;
+}
