1ac4081c2336970fb4d10daedc4c27f289cedb0c
adityakumar79915 <adityakumar79915@gmail.com>
2024-01-31 19:11:35 +0530

    updated

---
+++ Array/Arr_fun.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+void printArr(int arr[],int size){
+    cout<<"printing the array"<<endl;
+    for(int i=0; i<size; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<"printing done"<<endl;
+}
+int main(){
+    int first[10] = {1,4,6};
+    int n =10;
+    printArr(first,10);
+} 
