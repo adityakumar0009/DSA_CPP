97f7fbe626df3c3fa0b36c249567b0e53bf78acb
adityakumar79915 <adityakumar79915@gmail.com>
2024-05-06 01:15:06 +0530

    updated

---
+++ STL/deque.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<deque>
+using namespace std;
+int main(){
+    deque<int> d;
+    d.push_back(1);
+    d.push_front(2);
+    for(int i:d){
+        cout<<i<<" ";
+    }
+    cout<<"first element index"<<d.at(1)<<endl;
+    cout<<endl;
+    d.pop_front();
+    for(int i:d){
+        cout<<i<<" ";
+    }
+}
