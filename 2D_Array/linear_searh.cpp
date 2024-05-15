0e0fc541301034101e55c37cf588f6b88c88634d
adityakumar79915 <adityakumar79915@gmail.com>
2024-05-15 12:59:57 +0530

    updated

--- 2D_Array/linear_searh.cpp
+++ 2D_Array/linear_searh.cpp
@@ -1,14 +1,14 @@
 #include<iostream>
 using namespace std;
-bool ispresent(int arr[][4],int target, int i,int j){
+bool ispresent(int arr[][4],int target,int i, int j){
     for(int i=0; i<3; i++){
         for(int j=0; j<4; j++){
-             if(arr[i][j]==target);{
+             if(arr[i][j]==target){
                 return 1;
              }
         }
-        return 0;
 }
+return 0;
 }
 int main(){
     int arr[3][4];
@@ -32,6 +32,7 @@ int main(){
         cout<<"element found"<<endl;
     }
     else{
-        cout<<"not found";
+        cout<<"not found"<<endl;;
     }
+    return 0;
 }
