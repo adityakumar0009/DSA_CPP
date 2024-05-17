abc0769a2c3a25d7e6f82ec3fa151b0870123cdd
adityakumar79915 <adityakumar79915@gmail.com>
2024-05-17 13:45:08 +0530

    updated

--- pointer/function.cpp
+++ pointer/function.cpp
@@ -6,6 +6,16 @@ void print(int *p){
 void update(int *p){
     *p = *p+1;
 }
+
+int getsum(int arr[], int n){//*arr = arr[] dono same hai baat barabar hai
+    cout<<endl;
+    cout<<"size of -> "<<sizeof(arr)<<endl;
+    int sum =0;
+    for(int i=0; i<n; i++){
+        sum =sum + arr[i];
+    }
+    return sum;
+}
 int main(){
     int value = 5;
     int *p = &value;
@@ -13,5 +23,9 @@ int main(){
     cout<<"value before -> "<<*p<<endl;
     update(p);
     cout<<"after value -> "<<*p<<endl;
+
+    int arr[5] = {1,2,3,4,5};
+    cout<<"sum of array - > "<<getsum(arr,5);
+
     return 0;
 }
