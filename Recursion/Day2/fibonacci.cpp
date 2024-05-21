79b3d8ab689966093add441578ec5ba5f9488218
adityakumar79915 <adityakumar79915@gmail.com>
2024-05-21 13:52:14 +0530

    updated

---
+++ Recursion/Day2/fibonacci.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int fibonacci(int n){
+    //base case
+    if(n==0){
+        return 0;
+    }
+    if(n==1){
+        return 1;
+    }
+    int ans = fibonacci(n-1) + fibonacci(n-2);
+    return ans;
+}
+int main(){
+    int n= 8;
+    //cout<<"enter the number->"<<endl;
+    int ans = fibonacci(n);
+    cout<<ans<<endl;
+}
