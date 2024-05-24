74297e5d90ec5c7940a31b2a641fb8b9b6327dab
adityakumar79915 <adityakumar79915@gmail.com>
2024-05-24 13:06:56 +0530

    updated

--- Recursion/DAy4/bubble_sort.cpp
+++ Recursion/DAy4/bubble_sort.cpp
@@ -5,12 +5,13 @@ void sortedArray(int arr[], int n){
     if(n==0 || n==1){
         return ;
     }
-    //recursive call
+    //ek case solve kiya
     for(int i =0; i<n-1; i++){
         if(arr[i]>arr[i+1]){
             swap(arr[i],arr[i+1]);
         }
     }
+      //recursive call
     sortedArray(arr,n-1);
 }
 int main(){
