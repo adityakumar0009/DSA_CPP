a63a6220bd9e4d0605432fef41e91162b42665d4
adityakumar79915 <adityakumar79915@gmail.com>
2024-07-18 16:51:37 +0530

    updated

---
+++ Practice_question/Day_1/cal_to_fah.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float calcius;
+    cin>>calcius;
+    float fahrenheit = (9*calcius)/5+32;
+    cout<<fahrenheit<<endl;
+    return 0;
+}
---
+++ Practice_question/Day_1/fah_to_cal.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float fahrenheit;
+    cin>>fahrenheit;
+    float calcius = 5.0/9*(fahrenheit-32);
+    cout<<calcius<<endl;
+}
