061606e5d78348d9376c64fff544d9e2c3f49db7
adityakumar79915 <adityakumar79915@gmail.com>
2024-07-28 19:48:36 +0530

    updated

--- Practice_question/Day_8/duplicate.cpp
+++ Practice_question/Day_8/duplicate.cpp
@@ -5,7 +5,7 @@ int duplicates(int arr[],int size){
     for(int i=0; i<size; i++){
         ans = ans^arr[i];
     }
-    for(int i=0; i<size; i++){
+    for(int i=1; i<size; i++){
         ans = ans^i;
     }
     return ans;
---
+++ Practice_question/Day_8/intersection.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int intersection(int arr[], int n, int m, int brr[]) {
+    int i = 0;
+    int j = 0;
+    int ans = 0;
+    while (i < n && j < m) {
+        if (arr[i] == brr[j]) {
+            cout<<arr[i]<<" ";
+            // ans = arr[i];
+            i++;
+            j++;
+        } else if (arr[i] < brr[j]) {
+            i++;
+        } else {
+            j++;
+        }
+    }
+    return ans;
+}
+
+int main() {
+    int arr1[4] = {1, 2, 3, 4};
+    int arr2[4] = {3, 4, 5, 6};
+    intersection(arr1, 4, 4, arr2);
+    // cout  << ans << endl;
+    return 0;
+}
