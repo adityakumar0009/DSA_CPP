bca9dfdbdabc7b90ee7015cb2dcbdb8ee823525b
adityakumar79915 <adityakumar79915@gmail.com>
2024-07-29 22:32:23 +0530

    updated

---
+++ Practice_question/Day_10/stl_list.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<list>
+using namespace std;
+int main(){
+    list<int> l;
+    l.push_back(2);
+    l.push_front(1);
+    for(int i:l){
+        cout<<i<<" ";
+    }
+    cout<<endl;
+    l.pop_back();
+    for(int i:l){
+        cout<<i<<" ";
+    }
+    return 0;
+}
---
+++ Practice_question/Day_10/stl_stack.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+int main(){
+    stack<int> a;
+    a.push(3);
+    cout<<a<<endl;
+    return 0;
+}
