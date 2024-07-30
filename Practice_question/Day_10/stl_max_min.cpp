c2d892dc2ba2b3d2552910ba7aac06dc764b5995
adityakumar79915 <adityakumar79915@gmail.com>
2024-07-30 15:50:19 +0530

    updated

---
+++ Practice_question/Day_10/stl_max_min.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main(){
+    int a = 9;
+    int b = 11;
+    cout<<"max from a and b is->"<<max(a,b)<<endl;
+    cout<<"min from a and b is->"<<min(a,b)<<endl;
+    return 0;
+}
