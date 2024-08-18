27a41c1780e86196db89086f082187b53f3eb411
adityakumar79915 <adityakumar79915@gmail.com>
2024-08-19 01:14:42 +0530

    updated

---
+++ Practice_question/Day_19/macro.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+#define PI 3.14
+int main(){
+    int radius = 5;
+    // double PI = 3;
+    int area = PI*radius*radius;
+    cout<<"area is->"<<area;
+    return 0;
+}
