5214600e9be8b3b1970b0d6f378cfd31036c7fc3
adityakumar79915 <adityakumar79915@gmail.com>
2024-09-11 22:58:34 +0530

    updated

---
+++ Practice_question/Array/smallest.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int nums[] = {4,3,6,2,-11};
+    int size = 5;
+    int smallest = INT64_MAX;
+    for(int i=0; i<size; i++){
+        if(nums[i]<smallest)
+        smallest = nums[i];
+    }
+    cout<<"the smallest number is->"<<smallest;
+    return 0;
+}
