44d12a4ade32265232b557f041a28ed5e78a3f56
adityakumar79915 <adityakumar79915@gmail.com>
2024-09-11 23:05:17 +0530

    updated

---
+++ Practice_question/Array/largest.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int nums[] = {2,4,5,1,0};
+    int size = 5;
+    int largest = INT64_MIN;
+    for(int i=0; i<size; i++){
+        if(nums[i]>largest){
+            largest = nums[i];
+        }
+    }
+    cout<<"the largest number is->"<<largest<<endl;
+    return 0;
+}
--- Practice_question/Array/smallest.cpp
+++ Practice_question/Array/smallest.cpp
@@ -5,7 +5,7 @@ int main(){
     int size = 5;
     int smallest = INT64_MAX;
     for(int i=0; i<size; i++){
-        if(nums[i]<smallest)
+        if(nums[i]<smallest) //smallest = min(nums[i],smallest);
         smallest = nums[i];
     }
     cout<<"the smallest number is->"<<smallest;
