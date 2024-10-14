ef86e64a07f9b64c87f6a194a5455a9d91fcc4f8
adityakumar79915 <adityakumar79915@gmail.com>
2024-10-14 18:49:21 +0530

    updated

---
+++ Stack/celebrity_problem.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<vector>
+#include<stack>
+using namespace std;
+bool knows(vector<vector<int>>& M, int a, int b) {
+    return M[a][b] == 1;
+}
+int find_celebrity(vector<vector<int>>& M, int n){
+    stack<int> s;
+    for(int i=0; i<n; i++){
+        s.push(i);
+    }
+    while(s.size()>1){
+        int a = s.top();
+        s.pop();
+        int b = s.top();
+        s.pop();
+        if(knows(M,a,b)){
+            s.push(b);
+        }
+        else{
+            s.push(a);
+        }
+    }
+    int pot_celeb = s.top();
+    for(int i=0; i<n; i++){
+        if (i != pot_celeb && (knows(M,pot_celeb, i) || !knows(M,i, pot_celeb))) {
+            return -1; 
+        }
+    }
+    return pot_celeb;
+}
+using namespace std;
+int main(){
+    vector<vector<int>> M = {
+        {0,1,0},
+        {0,0,0},
+        {0,1,0}
+    };
+    int n = M.size();
+    int celebrity = find_celebrity(M,n);
+    if(celebrity==-1){
+        cout<<"celebrity not found"<<endl;
+    }
+    else{
+        cout<<"celebrity found"<<endl;
+    }
+    return 0;
+}
--- Stack/largest_area_hist.cpp
+++ Stack/largest_area_hist.cpp
@@ -1,6 +1,26 @@
 #include<iostream>
 #include<stack>
+#include<climits>
 using namespace std;
+int largest_area_his(int arr[], int n){
+    stack<int> s;
+    int next[arr,n];
+    int prev[arr,n];
+    int area = INT_MIN;
+    for(int i=0; i<n; i++){
+        int l = arr[i];
+        if (next[i] == -1) {
+            next[i] = n;
+        }
+        int w = next[i]-prev[i]-1;
+        int newArea = l*w;
+        area = max(area,newArea);
+    }
+    return area;
+}
 int main(){
+    int n = 6;
+    int arr[] = {2,1,5,6,2,3};
+    cout<<largest_area_his(arr,n);
     return 0;
 }
