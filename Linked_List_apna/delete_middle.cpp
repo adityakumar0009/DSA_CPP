8d79827d210b1a17567183c87055b059faf8c7f7
adityakumar79915 <adityakumar79915@gmail.com>
2025-01-04 11:49:32 +0530

    updated

--- Linked_List_apna/delete_middle.cpp
+++ Linked_List_apna/delete_middle.cpp
@@ -50,3 +50,28 @@ int main(){
     printlist(del);
     return 0;
 }
+
+// class Solution
+// {
+// public:
+//     ListNode *deleteMiddle(ListNode *head)
+//     {
+//         if (head == NULL || head->next == NULL)
+//         {
+//             delete head;
+//             return NULL;
+//         }
+//         ListNode *fast = head;
+//         ListNode *slow = head;
+//         ListNode *prev = NULL;
+//         while (fast != NULL && fast->next != NULL)
+//         {
+//             prev = slow;
+//             slow = slow->next;
+//             fast = fast->next->next;
+//         }
+//         prev->next = slow->next;
+//         delete slow;
+//         return head;
+//     }
+// };
---
+++ Linked_List_apna/linked_list_cycle.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+class ListNode{
+    public:
+    int data;
+    ListNode* next;
+    ListNode(int val){
+        data = val;
+        next = NULL;
+    }
+};
+class solution{
+    public:
+    bool hascycle(ListNode* head){
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast!=NULL && fast->next!=NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow==fast){
+                return true;
+            }
+        }
+        return false;
+    }
+};
+void printlist(ListNode* head){
+    while (head!=NULL)
+    {
+        cout<<head->data<<" ";
+        head= head->next;
+    }
+    cout<<endl;
+}
+int main(){
+    ListNode* head = new ListNode(3);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(0);
+    head->next->next->next = new ListNode(-4);
+    head->next->next->next->next = head->next;
+    solution sol;
+    if (sol.hascycle(head))
+    {
+        cout << "The list has a cycle." << endl;
+    }
+    else
+    {
+        cout << "The list does not have a cycle." << endl;
+    }
+
+    head->next->next->next->next = NULL; // Break the cycle
+    delete head->next->next->next;
+    delete head->next->next;
+    delete head->next;
+    delete head;
+    return 0;
+}
