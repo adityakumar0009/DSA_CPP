566f11c7d11e7a71bce4bdff37fabf65f571c26a
adityakumar79915 <adityakumar79915@gmail.com>
2025-01-04 15:05:15 +0530

    updated

--- Linked_List_apna/linked_list_cycle_2.cpp
+++ Linked_List_apna/linked_list_cycle_2.cpp
@@ -41,6 +41,7 @@ int main(){
     head->next = new ListNode(2);
     head->next->next = new ListNode(0);
     head->next->next->next = new ListNode(-4);
+    head->next->next->next->next = head->next;
     solution sol;
     ListNode *cycleStart = sol.detect_cycle(head);
     if(cycleStart){
