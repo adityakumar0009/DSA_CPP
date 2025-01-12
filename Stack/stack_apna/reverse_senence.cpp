3c99cee9b97e352478ed6e3b03a53a68ced81dfe
adityakumar79915 <adityakumar79915@gmail.com>
2025-01-13 00:24:49 +0530

    updated

---
+++ Stack/stack_apna/reverse_senence.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+void rev_sen(string s){
+    stack<string> st;
+    for(int i=0; i<s.length(); i++){
+        string words = " ";
+        while(s[i]!=' ' && s.length()){
+            words+=s[i];
+            i++;
+        }
+        st.push(words);
+
+    }
+    while(!st.empty()){
+        cout<<st.top()<<" ";
+        st.pop();
+    }
+    cout<<endl;
+}
+int main(){
+    string s = "hello,i am aditya.";
+    rev_sen(s);
+    return 0;
+}
