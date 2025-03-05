520eb9ebe3e18ef8c07be308aa39b64621ca628a
adityakumar79915 <adityakumar79915@gmail.com>
2025-03-05 23:01:52 +0530

    updated

---
+++ Dsa_sheet/Array/max_subarray_kandaes.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+#include<climits>
+using namespace std;
+int max_sub_sum(vector<int> &nums){
+    int curr_sum = 0;
+    int max_sum = INT_MIN;
+    for(int i=0; i<nums.size(); i++){
+        curr_sum+=nums[i];
+        max_sum = max(curr_sum,max_sum);
+
+        if(curr_sum<0){
+            curr_sum = 0;
+        }
+    }
+    return max_sum;
+}
+int main(){
+    vector<int> nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    cout<<"max_subarray is -> "<<max_sub_sum(nums);
+    return 0;
+}
