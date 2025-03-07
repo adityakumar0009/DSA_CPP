8568a48e99d20822624ea6159a3d507c6c49b610
adityakumar79915 <adityakumar79915@gmail.com>
2025-03-07 16:38:24 +0530

    updated

---
+++ Dsa_sheet/Linked_List/rev_linked_list.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+class ListNode{
+    public:
+    int data;
+    ListNode* next;
+    ListNode(int val){
+        data = val;
+        next = NULL;
+    }
+};
+class Solution{
+    public:
+    ListNode* revList(ListNode* head){
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        ListNode* next = NULL;
+        while(curr!=NULL){
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+};
+void printList(ListNode* head){
+    while(head!=NULL){
+        cout<<head->data<<" ";
+        head = head->next;
+    }
+    cout << endl;
+}
+int main(){
+    ListNode *head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    cout << "original list ";
+    printList(head);
+    // reverse list
+    Solution sol;
+    ListNode *reversedHead = sol.revList(head);
+    // Print the reversed list
+    cout << "Reversed list: ";
+    printList(reversedHead);
+    return 0;
+}
