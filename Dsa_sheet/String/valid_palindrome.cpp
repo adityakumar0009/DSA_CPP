8bd90c415d8628b03dd5e63bd6f9329b922d9f0a
adityakumar79915 <adityakumar79915@gmail.com>
2025-03-16 16:08:25 +0530

    updated

---
+++ Dsa_sheet/String/valid_palindrome.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+bool alpha_num(char ch){
+    if((ch>='0' && ch<='9') || (tolower(ch)<='a' && tolower(ch)>='z')){
+        return true;
+    }
+    return false;
+}
+bool is_palindrome(string s){
+    int st = 0;
+    int end = s.size()-1;
+    while(st<=end){
+        if(!alpha_num(s[st])){
+            st++;
+            continue;
+        }
+        if(!alpha_num(s[end])){
+            end--;
+            continue;
+        }
+        if(tolower(s[st])!=tolower(s[end])){
+            st++;
+            end--;
+        }
+    }
+    return true;
+}
+int main(){
+    string s  = "A man, a plan, a canal: Panama";
+    if(is_palindrome(s)){
+        cout<<"it is palindrome"<<endl;
+    }
+    else{
+        cout<<"it is not a plaindrome";
+    }
+    return 0;
+}
