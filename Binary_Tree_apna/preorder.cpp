1ff3d3a5785d8a41491ba0d660f0fb138657f07d
adityakumar79915 <adityakumar79915@gmail.com>
2025-05-01 08:51:23 +0530

    updated

---
+++ Binary_Tree_apna/preorder.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val){
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+static int index = -1;
+Node* buildTree(vector<int>& preorder){
+    index++;
+    if(preorder[index]==-1){
+        return NULL;
+    }
+    Node* root = new Node(preorder[index]);
+    root->left = buildTree(preorder);
+    root->right = buildTree(preorder);
+    return root;
+}
+//preorder
+void preOrder(Node* root){
+    if(root==NULL){
+        return ;
+    }
+    cout<<root->data<<" ";
+    preOrder(root->left);
+    preOrder(root->right);
+}
+int main(){
+    vector<int> preorder = {1, 2, -1, -1, 3, 4, -1, -1, 5, -1, -1};
+    Node *root = buildTree(preorder);
+    preOrder(root);
+    return 0;
+}
