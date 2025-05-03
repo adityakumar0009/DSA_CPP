70f1fa54561250b04518eca1559aee4ee0608989
adityakumar79915 <adityakumar79915@gmail.com>
2025-05-03 13:33:03 +0530

    updated

---
+++ Binary_Tree_apna/diameter_tree.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val){
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+static int index = -1;
+Node* buildTree(vector<int>& preorder){
+    index++;
+    if(preorder[index]==-1){
+        return NULL;
+    }
+    Node* root = new Node(preorder[index]);
+    root->left = buildTree(preorder);
+    root->right = buildTree(preorder);
+    return root;
+}
+int height(Node* root){
+    if(root==NULL){
+        return 0;
+    }
+    int left_height = height(root->left);
+    int right_height = height(root->right);
+    return max(left_height,right_height)+1;
+}
+int diameter(Node* root){
+    if(root==NULL){
+        return 0;
+    }
+    int left_diameter = diameter(root->left);
+    int right_diameter = diameter(root->right);
+    int curr_diameter = height(root->left) + height(root->right);
+    return max(curr_diameter,max(left_diameter,right_diameter));
+}
+int main(){
+    vector<int> preorder = {1, 2, -1, -1, 3, 4, -1, -1, 5, -1, -1};
+    Node *root = buildTree(preorder);
+    cout<<diameter(root);
+    return 0;
+}
