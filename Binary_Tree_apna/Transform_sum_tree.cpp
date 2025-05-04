aff45ddfbbd1832f13f7ea54d0acf6cbd350901c
adityakumar79915 <adityakumar79915@gmail.com>
2025-05-04 14:36:58 +0530

    updated

--- Binary_Tree_apna/Transform_sum_tree.cpp
+++ Binary_Tree_apna/Transform_sum_tree.cpp
@@ -23,17 +23,31 @@ Node* buildTree(vector<int> &preorder){
     root->right = buildTree(preorder);
     return root;
 }
+void preOrder(Node* root){
+    if(root==NULL){
+        return;
+    }
+    cout<<root->data<<" ";
+    preOrder(root->left);
+    preOrder(root->right);
+}
 int sum_tree(Node* root){
     if(root==NULL){
         return 0;
     }
     int left_sum = sum_tree(root->left);
     int right_sum = sum_tree(root->right);
-    return left_sum+right_sum+root->data;
+    root->data+=left_sum+right_sum;
+    return root->data;
 }
 int main(){
     vector<int> preorder = {1, 2, -1, -1, 3, 4, -1, -1, 5, -1, -1};
     Node *root = buildTree(preorder);
-    cout<<sum_tree(root);
+    cout<<"before conversion"<<endl;
+    preOrder(root);
+    cout<<endl;
+    sum_tree(root);
+    cout<<"after conversion"<<endl;
+    preOrder(root);
     return 0;
 }
