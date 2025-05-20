8fa6a031fee98d293782c0d8b8bde0e056d5a9ce
adityakumar79915 <adityakumar79915@gmail.com>
2025-05-21 00:07:11 +0530

    updated

---
+++ Binary_Search_Tree/basic.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* right;
+    Node* left;
+    Node(int val){
+        data = val;
+        right = NULL;
+        left = NULL;
+    }
+};
+Node* insert(Node* root,int val){
+    if(root==NULL){
+        return new Node(val);
+    }
+    if(val<root->data){
+        root->left = insert(root->left,val);
+    }
+    else{
+        root->right = insert(root->right,val);
+    }
+    return root;
+}
+Node* buildBst(vector<int>& arr){
+    Node* root = NULL;
+    for(int val:arr){
+        root = insert(root,val);
+    }
+    return root;
+}
+void inorder(Node* root){
+    if(root==NULL){
+        return;
+    }
+    inorder(root->left);
+    cout<<root->data<<" ";
+    inorder(root->right);
+}
+int main(){
+    vector<int> arr = {3,2,1,5,6,4};
+    Node* root = buildBst(arr);
+    inorder(root);
+    return 0;
+}
