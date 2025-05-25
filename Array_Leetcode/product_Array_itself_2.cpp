0109c8ae9ddd3dc0cd0d98645ae837d835487582
adityakumar79915 <adityakumar79915@gmail.com>
2025-05-25 23:11:17 +0530

    updated

---
+++ Array_Leetcode/product_Array_itself_2.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+vector<int> product_itself(vector<int>& nums){
+    int n = nums.size();
+    vector<int> ans(n,1);
+    vector<int> prefix(n,1);
+    vector<int> suffix(n,1);
+    prefix[0] = 1;
+    for(int i=1; i<n; i++){
+        prefix[i] = prefix[i-1]*nums[i-1];
+    }
+    suffix[n-1] = 1;
+    for(int j=n-2; j>=0; j--){
+        suffix[j] = suffix[j+1]*nums[j+1];
+    }
+    for(int i=0; i<n; i++){
+        ans[i]= prefix[i]*suffix[i];
+    }
+    return ans;
+}
+
+int main(){
+    vector<int> nums = {1, 2, 3, 4};
+    vector<int> result = product_itself(nums);
+    for(int val:result){
+        cout<<val<<" ";
+    }
+    return 0;
+}
