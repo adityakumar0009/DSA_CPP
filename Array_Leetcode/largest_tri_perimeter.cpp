4617b22825e7ba31784cde52397d3e037ca0318b
adityakumar79915 <adityakumar79915@gmail.com>
2025-05-26 18:00:04 +0530

    updated

---
+++ Array_Leetcode/largest_tri_perimeter.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int largest_perimeter(vector<int>& nums){
+    int n = nums.size();
+    sort(nums.begin(),nums.end());
+    for(int i=n-3; i>=0; i--){
+        if(nums[i]+nums[i+1]>nums[i+2]){
+            return nums[i] + nums[i+1] + nums[i+2];
+        }
+        return 0;
+    }
+}
+int main(){
+    vector<int> nums = {2, 1, 2};
+    cout<<"largest triangle perimeter is "<<largest_perimeter(nums);
+    return 0;
+}
