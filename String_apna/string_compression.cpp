e5bb4185d778487a198edef58a8a4fd5fef09958
adityakumar79915 <adityakumar79915@gmail.com>
2025-06-05 15:40:59 +0530

    updated

---
+++ String_apna/string_compression.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int compress(vector<char>&chars){
+    int n = chars.size();
+    int index = 0;
+    for(int i=0; i<n;i ++){
+        char ch = chars[i];
+        int count = 0;
+        while(i<n && chars[i]==ch){
+            count++;
+            i++;
+        }
+        if(count==1){
+            chars[index++] = ch;
+        }
+        else{
+            chars[index++] = ch;
+            string str = to_string(count);
+            for(char dig : str){
+                chars[index++] = dig;
+            }
+        }
+        i--;
+    }
+    chars.resize(index);
+    return index;
+}
+int main(){
+    vector<char> chars = {'a', 'a', 'b', 'b', 'c', 'c', 'c'};
+    cout<<"compressed string is -> "<<compress(chars);
+    return 0;
+}
