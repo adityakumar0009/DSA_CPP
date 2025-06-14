35a4a28bf977345262a929c72d5dce4d1723fd88
adityakumar79915 <adityakumar79915@gmail.com>
2025-06-14 09:32:09 +0530

    updated

--- Array_Leetcode/bubble_sort.cpp
+++ Array_Leetcode/bubble_sort.cpp
@@ -2,10 +2,15 @@
 using namespace std;
 void bubble_sort(int arr[],int n){
     for(int i=0; i<n-1; i++){
+        bool isSwap = false;
         for(int j=0; j<n-i-1; j++){
             if(arr[j]>arr[j+1]){
                 swap(arr[j],arr[j+1]);
+                isSwap = true;
             }
+        }
+        if(!isSwap){
+            return;
         }
     }
 }
