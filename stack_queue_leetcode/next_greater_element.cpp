4d06ce1b5cd6006661bc5de59800d09297c60716
adityakumar79915 <adityakumar79915@gmail.com>
2025-06-24 19:15:29 +0530

    updated

---
+++ stack_queue_leetcode/next_greater_element.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+#include<stack>
+using namespace std;
+int main(){
+    vector<int> arr = {6,8,0,1,3};
+    vector<int> ans(arr.size());
+    //next greater element
+    stack<int> st;
+    for(int i=arr.size()-1; i>=0; i--){
+        while(st.size()>0 && st.top()<=arr[i]){
+            st.pop();
+        }
+        if(st.empty()){
+            ans[i] = -1;
+        }
+        else{
+            ans[i] = st.top();
+        }
+        st.push(arr[i]);
+    }
+    //print
+    for(int i=0; i<ans.size(); i++){
+        cout<<ans[i]<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
