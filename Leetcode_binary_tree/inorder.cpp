17fc2e2df166f3efe39e676edf44ba7eb93ccf4e
adityakumar79915 <adityakumar79915@gmail.com>
2025-07-02 15:24:07 +0530

    updated

---
+++ Leetcode_binary_tree/inorder.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val){
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+static int index = -1;
+Node* buildTree(vector<int>& preorder){
+    index++;
+    if(preorder[index]==-1){
+        return NULL;
+    }
+    Node* root = new Node(preorder[index]);
+    root->left = buildTree(preorder);
+    root->right = buildTree(preorder);
+}
+void inorder(Node* root){
+    if(root==NULL){
+        return;
+    }
+    inorder(root->left);
+    cout<<root->data<<" ";
+    inorder(root->right);
+}
+int main(){
+    vector<int> preorder = {1, 2, -1, -1, 3, 4, -1, -1, 5, -1, -1};
+    Node* root = buildTree(preorder);
+    inorder(root);
+    return 0;
+}
