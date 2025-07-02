6ed63d6ef7273fb319911a36a012a87d0f821171
adityakumar79915 <adityakumar79915@gmail.com>
2025-07-02 15:31:17 +0530

    updated

--- Leetcode_binary_tree/postorder.cpp
+++ Leetcode_binary_tree/postorder.cpp
@@ -1,4 +1,3 @@
-// preorder travel -> left,root,right
 #include<iostream>
 #include<vector>
 using namespace std;
@@ -22,18 +21,18 @@ Node* buildTree(vector<int>& preorder){
     Node* root = new Node(preorder[index]);
     root->left = buildTree(preorder);
     root->right = buildTree(preorder);
-}
-void inorder(Node* root){
+};
+void postorder(Node* root){
     if(root==NULL){
-        return;
+        return ;
     }
-    inorder(root->left);
+    postorder(root->left);
+    postorder(root->right);
     cout<<root->data<<" ";
-    inorder(root->right);
 }
 int main(){
     vector<int> preorder = {1, 2, -1, -1, 3, 4, -1, -1, 5, -1, -1};
     Node* root = buildTree(preorder);
-    inorder(root);
+    postorder(root);
     return 0;
 }
